a9d98536f6c6fbe184603f9e305f1ef94b005392
Gigi Stark <gfs22@zoo.cs.yale.edu>
2018-05-03 23:49:11 -0400

    11:50 5-3

---
+++ data_processing.py
@@ -0,0 +1,218 @@
+import numpy as np 
+import tensorflow as tf 
+import os
+import pickle as pk
+from gensim.models import KeyedVectors 
+import dircache
+import re
+
+import sklearn as sk
+
+from sklearn.preprocessing import LabelEncoder
+
+#Declare Paths
+DATA_PATH = "/home/accts/gfs22/LING_380/Data"
+EMBED_PATH = os.path.join(DATA_PATH, "Embeddings/word2vec/data")
+CORPUS_PATH = os.path.join(DATA_PATH, "Corpus")
+SAVE_PATH = os.path.join(DATA_PATH, "Extracted")
+LEX_PATH = os.path.join(DATA_PATH, "Embeddings/lexicon.txt")
+
+#Set Constants
+MAX_THRESHOLD = 600000
+NUM_VOCAB = 3954340
+sent_length = 32
+embed_size = 300 
+
+ignore = {"@", "--", "-- ", " --"}
+dashes = r"[- ]*"
+
+#Function to Load Embeddings
+def load_embeddings():
+	print("EMBEDDING START")
+	model = KeyedVectors.load_word2vec_format(EMBED_PATH, binary=True)
+	print("EMBEDDING STOP")
+	return model
+
+#Function to remove dashes from words and convert words to lower case
+def format_word(word):
+	return re.sub(dashes, "", word.lower())
+
+#Function to use lexicon to initialize embedding matrix, list of POS, label encoder for POS, 
+#and dictionary that maps words to their embeddings
+def read_lex(embeddings):
+
+	#Open Lexicon
+	with open(LEX_PATH) as fh:
+		lines = fh.readlines()
+
+	num_words = NUM_VOCAB
+
+	#Initialize Embedding Matrix to Random Normally Distributed Numbers
+	embed_mat = np.random.normal(size=(num_words, 300))
+	poss = []
+	words = {}
+
+	#Generate list of all unique POS
+	#Fill in embed_mat appropriately
+	#Fill in words, a dictionary that maps words to actual embeddings
+	for line in lines:
+		word_list = line.strip().split("\t")
+		if len(word_list) == 5:
+			wid, _, word, _, pos = word_list[:5]
+			word = format_word(word)
+			wid = int(wid)
+			pos = format_POS(pos)
+			if word not in words:
+				words[word] = wid
+			poss.append(pos)
+			if word in embeddings:
+				embed_mat[wid, :] = embeddings[word]
+		else:
+			poss.append("N/A")
+
+	#Fit Label Encoder to part of speech tags
+	le = LabelEncoder()
+	le.fit(poss)
+
+	return le, poss, words, embed_mat
+
+#Function to generate indices for word embeddings (X_word), year (X_year), and POS tags for each file
+def read_in(file_name, le, updated_pos_tags, words):
+
+	#Extract the Year
+	split_fn = file_name.split("_")
+	X_year = int(split_fn[-2])
+
+	#Open file
+	with open(file_name) as fh:
+		lines = fh.readlines()
+
+	#Initialize variables
+	num_words = len(lines)
+
+	sent_X_word = []
+	sent_Y = []
+
+	X_word = np.zeros(shape=(sent_length,), dtype=np.int32)
+	Y = np.zeros(shape=(sent_length, ), dtype=np.int32)
+	counter = 0
+
+	#Print Name of File 
+	print(file_name)
+	for i, line in enumerate(lines):
+		word_list = line.strip().split("\t")
+
+		if len(word_list)==3 and word_list[0] not in ignore:
+
+			#Format word and pos
+			word, lemma, pos = word_list[:3]
+			pos = format_POS(pos)
+			word = format_word(word)
+
+			#Removes words that begin with @ and words not in vocabulary
+			if word.startswith("@") or word not in words:
+				continue
+
+			#Append indices for word embeddings to X_word and POS tags to Y for each word
+			if counter < sent_length:
+				X_word[counter] = words[word]
+				Y[counter] = le.transform([pos])[0]
+
+			#Begin a new sentence
+			if lemma == '.':
+				sent_X_word.append(X_word)
+				X_word = np.zeros(shape=(sent_length,), dtype=np.int32)
+				sent_Y.append(Y)
+				Y = np.zeros(shape=(sent_length, ), dtype=np.int32)
+				counter = 0
+
+			#Iterate counter if we are not beginning a new sentence
+			else:
+				counter += 1
+
+
+	#If we have sentences in a document join data from all sentences to make combined arrays
+	if sent_X_word!=[]:
+		X_word_array = np.stack(sent_X_word, axis=0)
+		X_year_array = np.repeat(X_year, len(sent_X_word))
+		Y_array = np.stack(sent_Y, axis=0)
+		return X_word_array, X_year_array, Y_array
+
+	#If there are no sentences in a document return all None values.
+	else:
+		return None, None, None
+
+#Format POS by taking all information before the first underscore
+#This gives us 423 POS
+format_POS = lambda pos: pos.split("_")[0]
+
+if __name__ == '__main__':
+
+	#Load Embeddings
+	embeddings = load_embeddings()
+
+	#Load the Lexicon
+	le, updated_pos_tags, words, embed_mat = read_lex(embeddings)
+
+	#Create a set with all the POS tags
+	updated_pos_tags = set(updated_pos_tags)
+
+	#Save Embedding MAtrix
+	with open(os.path.join(SAVE_PATH, "EMBED_MAT_FINAL_R.npz"), "wb") as fh:
+		np.save(fh, embed_mat)
+
+	#Initialize intermediate variables to store data from all files
+	X_word_arrays, X_year_arrays, Y_arrays = [], [], []
+
+	#Loop through at most 300 files from each decade
+	#Append results from each file to X_word_arrays, X_year_arrays, and Y_arrays
+	for dirpath, dirnames, filenames in os.walk(CORPUS_PATH):
+		for file in dirnames:
+			num = 0
+			file_path = os.path.join(CORPUS_PATH, file)
+			num_caches = len(dircache.listdir(file_path))
+			if num_caches>300:
+				filenames_chosen = np.random.choice(dircache.listdir(file_path), 300)
+			else:
+				filenames_chosen = dircache.listdir(file_path)
+			for filename in filenames_chosen:
+				print(num)
+				num += 1
+				X_word_array, X_year_array, Y_array = read_in(os.path.join(file_path, filename), le, updated_pos_tags, words)
+				if X_word_array is not None and X_year_array is not None and Y_array is not None:
+					X_word_arrays.append(X_word_array)
+					X_year_arrays.append(X_year_array)
+					Y_arrays.append(Y_array)
+
+	#Concatenate result along 0 axis
+	X_word_array = np.concatenate(X_word_arrays, axis=0)
+	X_year_array = np.concatenate(X_year_arrays, axis=0)
+	Y_array = np.concatenate(Y_arrays, axis=0)
+
+	#Save X_word_array
+	with open(os.path.join(SAVE_PATH, "X_word_array_FINAL_R.npz"), "wb") as fh:
+		np.save(fh, X_word_array)
+
+	#Save X_year_array
+	with open(os.path.join(SAVE_PATH, "X_year_array_FINAL_R.npz"), "wb") as fh:
+		np.save(fh, X_year_array)
+
+	#Save Y_array
+	with open(os.path.join(SAVE_PATH, "Y_array_FINAL_R.npz"), "wb") as fh:
+		np.save(fh, Y_array)
+	
+	#Only store indices in X_word for embeddings of 600000 most common words
+	indices = np.where(X_word_array>MAX_THRESHOLD)
+	X_word_array[indices]=0
+
+	#Only store embeddings of 600000 most common words
+	embed_mod = embed_mat[:MAX_THRESHOLD,]
+
+	#Save version of X_word_array with only indices for 600,000 most common words
+	with open(os.path.join(SAVE_PATH, "X_word_array_FINAL_TRIMMED_R.npz"), "wb") as fh:
+	 	np.save(fh, X_word_array)
+
+	#Save version of embedding matrix with only embeddings for 600,000 most common words
+	with open(os.path.join(SAVE_PATH, "EMBED_MAT_FINAL_TRIMMED_R.npz"), "wb") as fh:
+		np.save(fh, embed_mod)
+
--- lstm.py
+++ lstm.py
@@ -4,167 +4,441 @@ import tensorflow as tf
 import numpy as np
 import os, sys
 from tensorflow.contrib import rnn
+from NN_data_processing_updated import read_in
+from sklearn.manifold import TSNE
 
 # To stack more LSTM layers, just add more sizes to this list
 LAYERS = [512]
 
 # Maximum length of sentences
-MAX_LEN = 50
+MAX_LEN = 32 
 
 # Size of input embedding vectors
 EMBED_DIM = 300
 
 # Number of parts of speech
-N_POS = 423
+N_POS = 424
+
+#Declare Paths
+DATA_PATH = "/home/accts/gfs22/LING_380/Data/Extracted/"
+EMBED_PATH = "/home/accts/gfs22/LING_380/Data/Extracted/EMBED_MAT_FINAL_TRIMMED_R.npz" 
+TRAIN_SAVE_PATH = "/home/accts/gfs22/LING_380/Data/Full/Train"
+DEV_SAVE_PATH = "/home/accts/gfs22/LING_380/Data/Full/Dev"
+TEST_SAVE_PATH = "/home/accts/gfs22/LING_380/Data/Full/Test"
+MODEL_PATH = "/home/accts/gfs22/LING_380/Data/Output/FINAL_R"
+WRITE_TO_PATH = "/home/accts/gfs22/LING_380/Data/Output/FINAL_R.txt"
+X_WORD_FILENAME = "X_word_array_FINAL_TRIMMED_R.npz"
+X_YEAR_FILENAME = "X_year_array_FINAL_R.npz"
+Y_FILENAME = "Y_array_FINAL_R.npz"
+
+# Year embedding params
+START_YEAR = 1810
+END_YEAR = 2010
+NUM_YEAR = END_YEAR - START_YEAR
 
 # Hyperparameters
-LR = 0.01
-N_EPOCHS = 30
-BATCH_SIZE = 10
+LR = 0.001
+N_EPOCHS = 1
+BATCH_SIZE = 100 
+MAX_THRESHOLD = 600000
+
+#Function that will both print output to the terminal and write it to a file
+def log(content, input_data):
+    print input_data 
+    content.write(input_data)
+
 
 class Dataset:
 
-	""" Wrapper class for storing data. """
+    """ Wrapper class for storing data. """
+    def __init__(self, X_word, X_year, Y_label):
+        self.X_word = X_word
+        self.X_year = X_year
+        self.Y_label = Y_label
 
-	def __init__(self, X_word, X_year, Y_pred):
-		self.X_word = X_word
-		self.X_year = X_year
-		self.Y_pred = Y_pred
+    @staticmethod
+    def load(path, split):
 
-	@staticmethod
-	def load(self, path):
+        #Load Data from Data Processing to split in Train/Dev/Test
+        if split == 0:
+            X_word_path = os.path.join(path, X_WORD_FILENAME)
+            X_year_path = os.path.join(path, X_YEAR_FILENAME)
+            Y_path = os.path.join(path, Y_FILENAME)
 
-		X_word_path = os.path.join(path, "X_word")
-		X_word_path = os.path.join(path, "X_year")
-		X_word_path = os.path.join(path, "Y")
+        #Load Train/Dev/Test Data
+        else: 
+            X_word_path = os.path.join(path, "X_word_array_R.npz")
+            X_year_path = os.path.join(path, "X_year_array_R.npz")
+            Y_path = os.path.join(path, "Y_array_R.npz")
 
-		X_word_arrays = []
-		X_year_arrays = []
-		Y_arrays = []
+        X_word_array = np.load(X_word_path)
+        X_year_array = np.load(X_year_path)
+        Y_array = np.load(Y_path)
 
-		for dirpath, dirnames, filenames in os.walk(X_word_path):
-			for filename in filenames:
-				print("WORD" + filename)
-				X_word_array = np.load(os.path.join(X_word_path, filename))
-				X_word_arrays.append(X_word_array)
+        #Combined X_word_array, X_year_array, and Y_array to make a Dataset object
+        return Dataset(
+            X_word_array,
+            X_year_array,
+            Y_array 
+        )
 
-		for dirpath, dirnames, filenames in os.walk(X_year_path):
-			for filename in filenames:
-				print("YEAR" + filename)
-				X_year_array = np.load(os.path.join(X_year_path, filename))
-				X_year_arrays.append(X_year_array)
+    #Shuffle Data
+    def shuffle(self):
+        permutation = np.random.permutation(self.X_word.shape[0])
+        self.X_word = self.X_word[permutation, :]
+        self.X_year = self.X_year[permutation, ]
+        self.Y_label = self.Y_label[permutation, :]
 
-		for dirpath, dirnames, filenames in os.walk(Y_path):
-			for filename in filenames:
-				print("PATH" + filename)
-				Y_array = np.load(os.path.join(Y_path, filename))
-				Y_arrays.append(Y_array)
+    #Iterate Batches of Data
+    def iter_batches(self):
+        for i in xrange(0, len(self.X_word) - BATCH_SIZE, BATCH_SIZE):
+            yield (
+                i, 
+                self.X_word[i:i+BATCH_SIZE, :],
+                self.X_year[i:i+BATCH_SIZE, ],
+                self.Y_label[i:i+BATCH_SIZE, :],
+            )
 
-		return Dataset(
-			np.concatenate(X_word_arrays, axis=0),
-			np.concatenate(X_year_arrays, axis=0),
-			np.concatenate(Y_arrays, axis=0),
-		)
+    #Get Number of Batches
+    def get_n_batches(self):
+        return len(self.X_word) // BATCH_SIZE
 
-	def shuffle(self):
-		permutation = np.random.permutation(self.X_word.shape[0])
-		self.X_word = self.X_word[permutation, :, :]
-		self.X_year = self.X_year[permutation, :]
-		self.Y_pred = self.Y_pred[permutation, :]
+    #Save Function 
+    #Called Separately on Train, Dev, and Test Data
+    def save(self, savepath):
 
-	def iter_batches(self):
-		for i in xrange(0, len(self.X_word) - BATCH_SIZE, BATCH_SIZE):
-			yield (
-				self.X_word[i:i+BATCH_SIZE, :, :],
-				self.X_year[i:i+BATCH_SIZE, :],
-				self.Y_pred[i:i+BATCH_SIZE, :],
-			)
+        X_word_name = "X_word_array_R.npz"
+        X_year_name = "X_year_array_R.npz"
+        Y_name = "Y_array_R.npz"
 
-	def get_n_batches(self):
-		return len(self.X_word) // BATCH_SIZE
+        with open(os.path.join(savepath, X_word_name), "wb") as fh:
+            np.save(fh, self.X_word)
+
+        with open(os.path.join(savepath, X_year_name), "wb") as fh:
+            np.save(fh, self.X_year)
+
+        with open(os.path.join(savepath, Y_name), "wb") as fh:
+            np.save(fh, self.Y_label)
+
 
 
 class TemporalLanguageModel:
 
-	def add_graph(self):
+    #Create Graph Object
+    def add_graph(self):
 
-		self.X_word = tf.placeholder(tf.float32, [None, MAX_LEN, EMBED_DIM])
-		self.X_year = tf.placeholder(tf.float32, [None, MAX_LEN])
-		self.Y_pred = tf.placeholder(tf.int32, [None, MAX_LEN])
+        #Create placeholders for LSTM
+        self.X_word = tf.placeholder(tf.int32, [None, MAX_LEN])
+        self.X_year = tf.placeholder(tf.int32, [None])
+        self.Y_label = tf.placeholder(tf.int32, [None, MAX_LEN])
+        self.embedding_matrix = tf.placeholder(tf.float32, [MAX_THRESHOLD, EMBED_DIM])
 
-		X = tf.concat([self.X_word, tf.expand_dims(self.X_year, 2)], axis=2)
+        #Look up embeddings for each word
+        X_word = tf.nn.embedding_lookup(self.embedding_matrix, self.X_word)
 
-		rnn_layers = [rnn.LSTMCell(size) for size in LAYERS]
-		multi_rnn_cell = rnn.MultiRNNCell(rnn_layers)
+        #Create Year Embedding Layer
+        new_years = tf.subtract(self.X_year, START_YEAR)
+        unembedded_year = tf.tile(tf.expand_dims(new_years, axis=1), [1, MAX_LEN])
+        self.year_embed_mat = tf.get_variable(name="year_embed_mat", shape=(NUM_YEAR, EMBED_DIM), initializer=tf.contrib.layers.xavier_initializer())
+        embedded_year = tf.nn.embedding_lookup(self.year_embed_mat, unembedded_year)
 
-		H, _ = tf.nn.dynamic_rnn(
-			cell=multi_rnn_cell,
-			inputs=X, 
-			dtype=tf.float32
-		)
+        #Concatenate X_word and year embedding layer to get one input
+        X = tf.concat([X_word, embedded_year], axis=2)
 
-		self.Y = tf.contrib.layers.fully_connected(
-			inputs=H,
-			num_outputs=N_POS,
-		)
+        #Implement RNN
+        rnn_layers = [rnn.LSTMCell(size) for size in LAYERS]
+        multi_rnn_cell = rnn.MultiRNNCell(rnn_layers)
 
-		self.acc = tf.reduce_mean(tf.nn.top_k(self.Y_pred, k=1))
+        H, _ = tf.nn.dynamic_rnn(
+            cell=multi_rnn_cell,
+            inputs=X, 
+            dtype=tf.float32
+        )
 
-		log_p = tf.gather(tf.log(self.Y), self.Y_pred)
-		self.log_perp = -tf.reduce_mean(log_p)
+        #All POS
+        self.Y = tf.contrib.layers.fully_connected(
+            inputs=H,
+            num_outputs=N_POS,
+        )
+        
+        #Calculate Accuracy
+        equal = tf.equal(tf.cast(tf.argmax(self.Y, axis=2), tf.int32), tf.cast(self.Y_label, tf.int32))
+        self.acc = tf.reduce_mean(tf.cast(equal, tf.float32))
+        self.vec_acc = tf.reduce_mean(tf.cast(equal, tf.float32), axis=1)
 
-		self.loss = tf.losses.sparse_softmax_cross_entropy(
-			labels=self.Y_pred,
-			logits=self.Y,
-		)
+        #Calculate Log Perplexity
+        mask = tf.cast(tf.one_hot(self.Y_label, N_POS), tf.float32)
+        p = tf.reduce_sum(tf.nn.softmax(self.Y) * mask, axis=2)
+        self.log_perp = -tf.reduce_sum(tf.log(p), axis=1)/MAX_LEN
 
-		self.train_step = tf.train.AdamOptimizer(LR).minimize(self.loss)
+        #Calculate Loss
+        self.loss = tf.losses.sparse_softmax_cross_entropy(
+            labels=self.Y_label,
+            logits=self.Y,
+        )
 
-	def train(self, session, train_data, dev_data):
-		session.run(tf.global_variables_initializer())
-		n_batches = train_data.get_n_batches()
-		for i in xrange(N_EPOCHS):
-			train_data.shuffle()
-			loss = 0.
-			for batch_X_word, batch_X_year, batch_Y_pred in train_data.iter_batches():
-				d_loss, _ = session.run([self.loss, self.train_step], feed_dict={
-					self.X_word: batch_X_word,
-					self.X_year: batch_X_year,
-					self.Y_pred: batch_Y_pred,
-				})
-				loss += d_loss
-			print "#{} TRAIN : loss={:.3f}".format(i, loss / n_batches)
-			dev_loss, dev_acc, dev_log_perp = session.run([self.loss, self.acc, self.log_perp], feed_dict={
-				self.X_word: dev_data.X_word,
-				self.X_year: dev_data.X_year,
-				self.Y_pred: dev_data.Y_pred,
-			})
-			print "#{} DEV : loss={:.3f} : acc={:.3f} : log_perp={:.3f}".format(i, dev_loss, dev_acc, dev_log_perp)
+        #Set train_step that uses AdamOptimizer to minimize loss
+        self.train_step = tf.train.AdamOptimizer(LR).minimize(self.loss)
+
+
+    # def add_summaries(self):
+    #     tf.summary.scalar('loss', self.loss)
+    #     tf.summary.scalar('accuracy', self.acc)
+
+    #Function to train model
+    def train(self, session, train_data, dev_data, test_data, embed_data):
+        saver = tf.train.Saver()
+        session.run(tf.global_variables_initializer())
+        n_batches = train_data.get_n_batches()
+        merged = tf.summary.merge_all()
+        #train_writer = tf.summary.FileWriter( "/home/accts/gfs22/LING_380/Data/Output/Train_Summary", session.graph)
+        #dev_loss = float("inf")
+        #no_improve = 0
+
+    
+        data_file = WRITE_TO_PATH
+
+        content=open(data_file, "a")
+
+        #Loop through epochs
+        for i in xrange(N_EPOCHS):
+            log(content, "PRE-SHUFFLED\n")
+            log(content, str(i))
+            train_data.shuffle()
+            log(content, "SHUFFLED!!\n")
+            loss = 0.
+
+            #Loop through Batches
+            for j, batch_X_word, batch_X_year, batch_Y_label in train_data.iter_batches():
+                d_loss, _, d_merged = session.run([self.loss, self.train_step, merged], feed_dict={
+                    self.X_word: batch_X_word,
+                    self.X_year: batch_X_year,
+                    self.Y_label: batch_Y_label,
+                    self.embedding_matrix: embed_data
+                })
+                loss += d_loss
+                #train_writer.add_summary(d_merged, i*n_batches+j)
+                if j%5000==0:
+                    log(content, "#{}, {}, BATCH : loss={:.3f}\n".format(i, j, d_loss)) 
+            log(content, "#{} TRAIN : loss={:.3f}\n".format(i, loss / n_batches))
+           
+           # prev_dev_loss = dev_loss
+            # dev_loss, dev_acc, dev_log_perp = session.run([self.loss, self.acc, self.log_perp], feed_dict={
+            #     self.X_word: dev_data.X_word,
+            #     self.X_year: dev_data.X_year,
+            #     self.Y_label: dev_data.Y_label,
+            # })
+            #over sequence not part of speech!!
+
+            #Calculate Accuracy on Development Set at the end of the epoch
+            dev_acc = session.run(self.acc, feed_dict={
+                self.X_word: dev_data.X_word,
+                self.X_year: dev_data.X_year,
+                self.Y_label: dev_data.Y_label,
+                self.embedding_matrix : embed_data
+            })
+
+            #early stopping
+            # if dev_loss<prev_dev_loss:
+            #     no_improve=0
+            
+            # else:
+            #     no_improve+=1
+            
+            # if no_improve>3:
+            #     break
+
+            #log(content, "#{} DEV : loss={:.3f}\n".format(i, dev_loss))
+            log(content, "#{} DEV : acc={:.3f}\n".format(i, dev_acc)) 
+
+        content.close()
+        del train_data
+        del dev_data
+        saver.save(session, MODEL_PATH)
+
+    #Method to annotate clustered plots
+    @staticmethod
+    def annotate(image, words, n=float("inf")):
+        for i, (label, x, y) in enumerate(zip(words, image[:, 0], image[:, 1])):
+            if i == n: break
+            plt.annotate(
+                label,
+                xy=(x, y)
+            )
+
+    #Method to produce clustering plot
+    def clustering(self):
+        #Restore Model
+        sess = tf.Session()
+        saver = tf.train.Saver()
+        saver.restore(sess, MODEL_PATH)
+
+        #Extract year embedding from restored model
+        embedded_var = sess.run(self.year_embed_mat)
+
+        #Fit TSNE to year embedding
+        tsne = TSNE(n_components=2, metric="cosine")
+        image = tsne.fit_transform(embedded_var)
+        
+        #Plot Clusters
+        size = 50
+        colors = ["r", "g", "b", "y"]
+        for i in xrange(0, len(image), size):
+            plt.scatter(*zip(*image[i:i+size,:]), c=colors[int(i / size)])
+
+        #Annotate Clusters
+        TemporalLanguageModel.annotate(image, map(str, range(1810, 2010)))
+
+        #Show plot
+        plt.show()
+
+
+    #Calculate Test Accuracy
+    def test(self, test_data):
+        #Restore Model
+        sess = tf.Session()
+        saver = tf.train.Saver()
+        saver.restore(sess, MODEL_PATH)
+
+        #Calculate Test Accuracy 
+        test_acc = sess.run(self.acc, feed_dict={
+                self.X_word: test_data.X_word,
+                self.X_year: test_data.X_year,
+                self.Y_label: test_data.Y_label,
+        })
+        print('Test acc: {}'.format(test_acc))
+        return test_acc
+
+    def sample_sentence(self, X_word_array, Y_array):
+        #Restore Model
+        sess = tf.Session()
+        saver = tf.train.Saver()
+        saver.restore(sess, MODEL_PATH)
+
+        #Get inputs for sample sentence
+        years = np.arange(START_YEAR, END_YEAR)
+        X_word_array = np.tile(np.expand_dims(X_word_array, axis=0), [NUM_YEAR, 1])
+        Y_array = np.tile(np.expand_dims(Y_array, axis=0), [NUM_YEAR, 1])
+
+        #Calculate Entropy for input sentence over all years
+        metric = sess.run(self.log_perp, feed_dict={
+            self.X_word: X_word_array,
+            self.X_year: years,
+            self.Y_label: Y_array
+        })
+
+
+        #Plot Years vs. Entropy
+        plt.title(str(X_year))
+        plt.plot(years, metric)
+        plt.xlabel("Years")
+        plt.ylabel("Entropy")
+
+        #Show plot
+        plt.show()
+
+#Function to Divide data set into train, dev and test
+def cut_dataset(data_path):
+
+    #Load Data
+    data = Dataset.load(data_path, 0)
+
+    #Set Indices for Cutting
+    NUM_SENT = 750000
+    train_end= int(0.7*NUM_SENT)
+    dev_end = int(0.85*NUM_SENT)
+
+    #Shuffle data
+    data.shuffle()
+
+    #Subset train data
+    train_X_word = data.X_word[:train_end, :]
+    train_X_year = data.X_year[:train_end]
+    train_Y = data.Y_label[:train_end, :]
+
+    #Subset dev data
+    dev_X_word = data.X_word[train_end:dev_end, :]
+    dev_X_year = data.X_year[train_end:dev_end]
+    dev_Y = data.Y_label[train_end:dev_end, :]
+
+    #Subset test data
+    test_X_word = data.X_word[dev_end:NUM_SENT, :]
+    test_X_year = data.X_year[dev_end:NUM_SENT]
+    test_Y = data.Y_label[dev_end:NUM_SENT, :]
+
+    #Return train data object
+    train_data = Dataset(
+        train_X_word, 
+        train_X_year,
+        train_Y 
+    )
+
+    #Return dev data object
+    dev_data = Dataset(
+        dev_X_word, 
+        dev_X_year, 
+        dev_Y
+    )
+
+    #Return test data object
+    test_data = Dataset(
+        test_X_word, 
+        test_X_year,
+        test_Y 
+    )
+
+    #Save train, dev, and test data
+    train_data.save(TRAIN_SAVE_PATH)
+    dev_data.save(DEV_SAVE_PATH)
+    test_data.save(TEST_SAVE_PATH)
 
 def main():
 
-	# train_data = Dataset.load(TRAIN_PATH)
-	# dev_data = Dataset.load(DEV_PATH)
-	# test_data = Dataset.load(TEST_PATH)
+    #Create Train, Dev, and Test Data
+    cut_dataset(DATA_PATH)
 
-	train_data = Dataset(
-		np.random.uniform(size=[100, MAX_LEN, EMBED_DIM]),
-		np.random.uniform(size=[100, MAX_LEN]),
-		np.random.uniform(size=[100, MAX_LEN], high=100),
-	)
+    #Load Train, Dev, and Test Data
+    train_data = Dataset.load(TRAIN_SAVE_PATH, 1)
+    dev_data = Dataset.load(DEV_SAVE_PATH, 1)
+    test_data = Dataset.load(TEST_SAVE_PATH, 1)
 
-	dev_data = Dataset(
-		np.random.uniform(size=[100, MAX_LEN, EMBED_DIM]),
-		np.random.uniform(size=[100, MAX_LEN]),
-		np.random.uniform(size=[100, MAX_LEN], high=100),
-	)
+    #Load Embedding Matrix
+    if EMBED_PATH is not None:
+        with open(EMBED_PATH) as fh:
+            embed_data = np.load(fh)
+    else:
+        embed_data = None
 
-	session = tf.Session()
+    #Create a session
+    session = tf.Session()
 
-	model = TemporalLanguageModel()
-	model.add_graph()
-	model.train(session, train_data, dev_data)
+    print("Declare Model")
+    model = TemporalLanguageModel()
+
+    print("Adding Graph")
+    model.add_graph()
+
+   # print("Adding summaries")
+   # model.add_summaries()
+
+    print("Training!")
+    model.train(session, train_data, dev_data, test_data, embed_data)
+
+    print("Testing")
+    model.test(test_data)
+
+    print("Clustering")
+    model.clustering()
+    
+    print("Sample Sentence")
+    NUM_SENT = 750000
+    sample_indices = np.random.randint(int(0.15*NUM_SENT, size=(1, 15))
+    for index in sample_indices:
+        X_word_arr = test_data.X_word[index,:]
+        Y_arr = test_data.Y_label[index,:]
+        X_year = test_data.X_year[index]
+        model.sample_sentence(X_word_arr, Y_arr)
 
 
 if __name__ == "__main__":
-	main()
+    main()
